57e78458c5208a5719d1da5215b225495c95af65
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2023-02-02 01:19:31 +0300

    create-components

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,96 +1,30 @@
 import React from 'react'
 
-const Trending: React.FC = () => {
+const Trending: React.FC = ({trendings}) => {
+
   return (
     <div className="trending">
           <h2>Trending</h2>
           <div className="slider">
-            <div className="slide">
+            {trendings.map(obj => (<div className="slide">
               <div className="slide-bookmark">
                 <img src="./assets/icon-bookmark-empty.svg" alt="bookmark" />
               </div>
-              <img className="slide-img" src="./assets/thumbnails/beyond-earth/trending/large.jpg" alt=" " />
+              <img className="slide-img" src={obj.image} alt=" " />
               <div className="slide-info">
                 <div className="info">
-                  <p>2019 •</p>
+                  <p>{obj.year} •</p>
 
                   <img className="info-img" src="./assets/icon-category-movie.svg" alt=" " />
-                  <p>Movie •</p>
+                  <p>{obj.category} •</p>
 
                   <p>PG</p>
                 </div>
-                <h3 className="slide-title">Beyond Earth</h3>
+                <h3 className="slide-title">{obj.name}</h3>
               </div>
-            </div>
-            <div className="slide">
-              <div className="slide-bookmark">
-                <img src="./assets/icon-bookmark-empty.svg" alt="bookmark" />
-              </div>
-              <img className="slide-img" src="./assets/thumbnails/bottom-gear/trending/large.jpg" alt=" " />
-              <div className="slide-info">
-                <div className="info">
-                  <p>2019 •</p>
-
-                  <img className="info-img" src="./assets/icon-category-movie.svg" alt=" " />
-                  <p>Movie •</p>
-
-                  <p>PG</p>
-                </div>
-                <h3 className="slide-title">Beyond Earth</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slide-bookmark">
-                <img src="./assets/icon-bookmark-empty.svg" alt="bookmark" />
-              </div>
-              <img className="slide-img" src="./assets/thumbnails/undiscovered-cities/trending/large.jpg" alt=" " />
-              <div className="slide-info">
-                <div className="info">
-                  <p>2019 •</p>
-
-                  <img className="info-img" src="./assets/icon-category-movie.svg" alt=" " />
-                  <p>Movie •</p>
-
-                  <p>PG</p>
-                </div>
-                <h3 className="slide-title">Beyond Earth</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slide-bookmark">
-                <img src="./assets/icon-bookmark-empty.svg" alt="bookmark" />
-              </div>
-              <img className="slide-img" src="./assets/thumbnails/1998/trending/large.jpg" alt=" " />
-              <div className="slide-info">
-                <div className="info">
-                  <p>2019 •</p>
-
-                  <img className="info-img" src="./assets/icon-category-movie.svg" alt=" " />
-                  <p>Movie •</p>
-
-                  <p>PG</p>
-                </div>
-                <h3 className="slide-title">Beyond Earth</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slide-bookmark">
-                <img src="./assets/icon-bookmark-empty.svg" alt="bookmark" />
-              </div>
-              <img className="slide-img" src="./assets/thumbnails/dark-side-of-the-moon/trending/large.jpg" alt=" " />
-              <div className="slide-info">
-                <div className="info">
-                  <p>2019 •</p>
-
-                  <img className="info-img" src="./assets/icon-category-movie.svg" alt=" " />
-                  <p>Movie •</p>
-
-                  <p>PG</p>
-                </div>
-                <h3 className="slide-title">Beyond Earth</h3>
-              </div>
-            </div>
-
+            </div>))}
+            
+         
           </div>
         </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 import { SearchBar, Trending, Recomended, Header } from '../components';
 
 
 
 const Home: React.FC = () => {
+    const [trendings, setTrendings] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('https://63dafdb7b8e69785e479f6d2.mockapi.io/trending')
+        .then(res => setTrendings(res.data))
+    },[])
+
+    console.log(trendings)
+
     return (
         <>
             <Header />
             <div className="content">
                 <SearchBar />
-                <Trending />
+                <Trending trendings={trendings} />
                 <Recomended />
             </div>
         </>
